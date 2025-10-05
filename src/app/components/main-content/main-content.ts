bd5c631389770313e22f703f64f952aa2b8fe7e4
Erikson Gonçalves Bezerra <eriksongoncalves@yahoo.com.br>
2025-10-05 17:00:43 -0300

    styles: task list section

--- src/app/components/main-content/main-content.ts
+++ src/app/components/main-content/main-content.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 
 import { WelcomeSection } from '@components/welcome-section/welcome-section'
+import { TaskListSection } from '@components/task-list-section/task-list-section'
 
 @Component({
   selector: 'app-main-content',
-  imports: [WelcomeSection],
+  imports: [WelcomeSection, TaskListSection],
   templateUrl: './main-content.html',
   styleUrl: './main-content.css'
 })
