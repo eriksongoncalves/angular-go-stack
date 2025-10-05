1d0617a69cf1a4e1fb8d8102b8a7cce0229097da
Erikson Gonçalves Bezerra <eriksongoncalves@yahoo.com.br>
2025-10-05 17:52:24 -0300

    styles: task comments component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core'
 import { Header } from '@components/header/header'
 import { MainContent } from '@components/main-content/main-content'
 import { TaskFormModal } from '@components/task-form-modal/task-form-modal'
+import { TaskCommentsModal } from '@components/task-comments-modal/task-comments-modal'
 @Component({
   selector: 'app-root',
-  imports: [Header, MainContent, TaskFormModal],
+  imports: [Header, MainContent, TaskFormModal, TaskCommentsModal],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
