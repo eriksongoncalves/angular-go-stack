d56c4d207c6b18458f2b0c003d52091f001c257a
Erikson Gonçalves Bezerra <eriksongoncalves@yahoo.com.br>
2025-10-05 17:28:38 -0300

    styles: task form modal

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core'
 
 import { Header } from '@components/header/header'
 import { MainContent } from '@components/main-content/main-content'
+import { TaskFormModal } from '@components/task-form-modal/task-form-modal'
 @Component({
   selector: 'app-root',
-  imports: [Header, MainContent],
+  imports: [Header, MainContent, TaskFormModal],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
