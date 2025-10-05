42c81d72649795cb0fa35a5ac647cf727b49ac6b
Erikson Gonçalves Bezerra <eriksongoncalves@yahoo.com.br>
2025-10-05 14:33:22 -0300

    feat: initial components

---
+++ src/app/components/task-list-section/task-list-section.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-task-list-section',
+  imports: [],
+  templateUrl: './task-list-section.html',
+  styleUrl: './task-list-section.css'
+})
+export class TaskListSection {}
