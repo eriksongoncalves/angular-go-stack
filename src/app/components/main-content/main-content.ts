c9bd3e404174cd9a7299d8507f98dddf91e73268
Erikson Gonçalves Bezerra <eriksongoncalves@yahoo.com.br>
2025-10-05 15:30:10 -0300

    styles: main content component

--- src/app/components/main-content/main-content.ts
+++ src/app/components/main-content/main-content.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core'
 
+import { WelcomeSection } from '@components/welcome-section/welcome-section'
+
 @Component({
   selector: 'app-main-content',
-  imports: [],
+  imports: [WelcomeSection],
   templateUrl: './main-content.html',
   styleUrl: './main-content.css'
 })
