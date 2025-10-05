99502bfa7bdad49d295bf7241464d0ea967a1c00
Erikson Gonçalves Bezerra <eriksongoncalves@yahoo.com.br>
2025-10-05 17:11:11 -0300

    styles: task card component

--- src/app/components/task-list-section/task-list-section.ts
+++ src/app/components/task-list-section/task-list-section.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core'
 
+import { TaskCard } from '@components/task-card/task-card'
+
 @Component({
   selector: 'app-task-list-section',
-  imports: [],
+  imports: [TaskCard],
   templateUrl: './task-list-section.html',
   styleUrl: './task-list-section.css'
 })
